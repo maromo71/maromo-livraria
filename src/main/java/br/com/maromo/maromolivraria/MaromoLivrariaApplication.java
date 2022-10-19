a09650d4d95cc94dfea6827c88458f165ced1764
Maromo <professormoraes@gmail.com>
2022-10-19 15:59:20 -0300

    Primeiro commit do exemplo para aula de SD (API Rest com Spring Boot)

---
+++ src/main/java/br/com/maromo/maromolivraria/MaromoLivrariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.maromo.maromolivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaromoLivrariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaromoLivrariaApplication.class, args);
+    }
+
+}
